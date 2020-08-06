3fca5c186733fe083bad3e30f213e20d35f7dd78
Jason <47641725+Jfadelli@users.noreply.github.com>
2020-08-06 10:39:56 -0700

    Nice

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+require('dotenv').config()//if there is a .env on the rtoot folder it will merge it into process.env
+
 const express = require('express')
 
 const server = express()
 
 server.get('/', function (req, res) {
-    res.status(200).json({message: 'hello'})
+    const message = process.env.MESSAGE || 'hello from code';
+
+    res.status(200).json({message, database: process.env.DB_NAME})
 })
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8002;
 server.listen(PORT, () =>{
-    console.log(`\n*** Server Running on http://localhost:${port} ***\n`)
+    console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`)
 })
